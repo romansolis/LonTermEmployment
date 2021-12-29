077ed64567644cb6f7ca4b50fb61f0b72a86dead
romansolis <95838359+romansolis@users.noreply.github.com>
2021-12-29 08:48:16 +0800

    Add files via upload

---
+++ HomeWork-LonTermUnemployed/SQLLTURomanSolis.sql
@@ -0,0 +1,26 @@
+-- Create LTUStatistics Database
+-- Create LTU Table 
+
+select * from [dbo].[LTU]
+select * from [dbo].[tblFemale]
+
+Use [LTUstatistics]
+[dbo].[LTU]
+USE [LTUstatistics]
+
+
+CREATE TABLE tblMale(
+	[Age] [nvarchar](255) NULL,
+	[Gender] [nvarchar](255) NULL,
+	[Period] [datetime] NULL,
+	[Unemployed] [float] NULL
+) ON [PRIMARY]
+
+CREATE TABLE tblFemale(
+	[Age] [nvarchar](255) NULL,
+	[Gender] [nvarchar](255) NULL,
+	[Period] [datetime] NULL,
+	[Unemployed] [float] NULL
+) ON [PRIMARY]
+[Units][Unit Price]
+
